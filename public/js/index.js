db5e2315175a2ea7003d991213c059559ebff7b6
JesseRap <JRap@jesse-rap.local>
2017-06-23 11:31:08 -0400

    make serial animation
    move ball 1, delay, move ball 2

--- public/js/index.js
+++ public/js/index.js
@@ -1,193 +1,193 @@
-const BALL_RADIUS = 10;
-const WALL_SIZE = 400;
+const BALL_RADIUS = 20;
+const WALL_SIZE = 1000;
+const BALL_SPEED = 3;
+let LAUNCH_DELAY = INITAL_LAUNCH_DELAY = 60;
 
-const canvasWidth = window.innerWidth - 100;
-const canvasHeight = window.innerHeight - 100;
+// const canvasWidth = window.innerWidth - 100;
+// const canvasHeight = window.innerHeight - 100;
+const canvasWidth = 500;
+const canvasHeight = 500;
 
 
+// ********************** //
 
-// SET UP SCENE, CAMERA, RENDERER
+// SET UP SCENE, CAMERA, RENDERER, LIGHTS
 
+// SCENE
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, canvasWidth / canvasHeight, 0.1, 1000 );
-camera.position.set(0,20,-200);
-camera.lookAt(scene.position);
 
+const group = new THREE.Group();
+scene.add(group);
+
+// CAMERA
+const camera = new THREE.PerspectiveCamera( 30, canvasWidth / canvasHeight, 0.1, 1000 );
+camera.position.set(0,50,500);
+// camera.lookAt(scene.position);
+
+// RENDERER
 const renderer = new THREE.WebGLRenderer( {
   canvas: document.getElementById('three')
 });
 renderer.setSize( canvasWidth, canvasHeight );
 
-var axisHelper = new THREE.AxisHelper( 100 );
+// AIXSHELPER - provides visible orientation axes
+const axisHelper = new THREE.AxisHelper( 100 );
 scene.add( axisHelper );
+
+// LIGHTS
+var ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
+var pointLight = new THREE.PointLight(0xffffff, 1, 0.1)
+var pointLight2 = new THREE.PointLight(0xffffff)
+pointLight.position.set(0,50,100);
+pointLight2.position.set(0,50,500);
+scene.add(ambientLight);
+scene.add(pointLight);
+scene.add(pointLight2);
 
 
 // ********************** //
 
-// CUBE
-// const geometry = new THREE.BoxGeometry( 50, 50, 50 );
-// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-// const cube = new THREE.Mesh( geometry, material );
-// cube.position.set(0,40,40);
-// scene.add( cube );
+// MAKE FLOOR AND WALL
+const planeTexture = new THREE.TextureLoader().load('../img/checkerboard.png');
+// floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping;
+// floorTexture.repeat.set( 1, 1 );
+const planeMaterial = new THREE.MeshLambertMaterial({
+  map: planeTexture,
+  side: THREE.DoubleSide
+});
+const planeGeometry = new THREE.PlaneGeometry(WALL_SIZE, WALL_SIZE, 1, 1);
+
+const floor = new THREE.Mesh(planeGeometry, planeMaterial);
+const wall = new THREE.Mesh(planeGeometry, planeMaterial);
+wall.rotation.x = Math.PI / 2; // rotate the wall so it's perpendicular to floor
+
+group.add(floor);
+group.add(wall);
 
 
 
-let goingRight = true;
-function ballToRight(ball) {
-  if (goingRight && ball.position.x < 100) {
-    ball.position.x += 1
-  } else {
-    goingRight = false;
-    ball.position.x -= 1
-    if (ball.position.x < 0.5) {
-      goingRight = true;
-    }
+// ********************** //
+
+
+// MAKE SPHERES
+var sphereGeometry = new THREE.SphereGeometry(BALL_RADIUS, 30, 30);
+var sphere1Material = new THREE.MeshPhongMaterial({color: 'red'});
+var sphere2Material = new THREE.MeshPhongMaterial({color: 'blue'});
+const sphere1 = new THREE.Mesh(sphereGeometry, sphere1Material);
+const sphere2 = new THREE.Mesh(sphereGeometry, sphere2Material);
+
+const SPHERE2_X = -40;
+const SPHERE2_Y = 60;
+const SPHERE2_Z = 200;
+
+const SPHERE1_X = 20;
+const SPHERE1_Y = 30;
+const SPHERE1_Z = 270;
+
+sphere1.position.set(SPHERE1_X, SPHERE1_Y, SPHERE1_Z);
+sphere1.INITIAL_X = SPHERE1_X;
+sphere2.position.set(SPHERE2_X, SPHERE2_Y, SPHERE2_Z);
+sphere2.INITIAL_X = SPHERE2_X;
+group.add(sphere1);
+group.add(sphere2);
+
+// ********************** //
+
+// HELPER FUNCTIONS
+
+
+
+function animateCamera() {
+  if (camera.rotation.x > -Math.PI / 2) {
+    camera.rotation.x -= 0.01
   }
 }
 
 
-// var floorTexture = new THREE.ImageUtils.loadTexture( 'img/checkerboard.png' );
-// floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping;
-// floorTexture.repeat.set( 10, 10 );
-// DoubleSide: render texture on both sides of mesh
+const animations = ['ball1', 'delay', 'ball2', 'delay'];
+let currentAnimationIdx = 0;
+let currentAnimation = animations[currentAnimationIdx];
+
+function nextAnimation() {
+  currentAnimationIdx = (currentAnimationIdx + 1) % animations.length;
+  currentAnimation = animations[currentAnimationIdx];
+}
+
+function delay() {
+  if (LAUNCH_DELAY === 0) {
+    nextAnimation();
+    LAUNCH_DELAY = INITAL_LAUNCH_DELAY;
+  } else {
+    LAUNCH_DELAY -= 1;
+  }
+}
 
 
-// MAKE FLOOR AND WALL
-let floorTexture = new THREE.TextureLoader().load('../img/checkerboard.png');
-floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping;
-floorTexture.repeat.set( 1, 1 );
-var floorMaterial = new THREE.MeshLambertMaterial({
-  map: floorTexture,
-  side: THREE.DoubleSide
-});
-var floorGeometry = new THREE.PlaneGeometry(WALL_SIZE, WALL_SIZE, 1, 1);
-var floor = new THREE.Mesh(floorGeometry, floorMaterial);
-var wall = new THREE.Mesh(floorGeometry, floorMaterial);
-floor.position.set(0,0,0);
-wall.position.set(0,0,0);
-wall.rotation.x = Math.PI / 2;
-// floor.rotation.x = Math.PI / 2;
-scene.add(floor);
-scene.add(wall);
+sphere1.isLeavingOrigin = true;
+sphere1.destination = sphere1.INITIAL_X + 200;
+sphere2.isLeavingOrigin = true;
+sphere2.destination = sphere2.INITIAL_X - 200;
+
+function moveBallAway(ball, speed) {
+  ball.position.x = ball.position.x + (ball.destination > ball.INITIAL_X ? speed : -speed)
+}
+function moveBallBack(ball, speed) {
+  ball.position.x = ball.position.x + (ball.destination > ball.INITIAL_X ? -speed : speed)
+}
+
+
+function moveBall(ball) {
+  if (ball.isLeavingOrigin && Math.abs(ball.position.x - ball.destination) > 1) {
+    moveBallAway(ball, BALL_SPEED);
+  } else {
+    ball.isLeavingOrigin = false;
+    moveBallBack(ball, BALL_SPEED);
+    if (Math.abs(ball.position.x - ball.INITIAL_X) < 0.1) {
+      ball.isLeavingOrigin = true;
+      nextAnimation();
+    }
+  }
+}
+
+function doAnimation() {
+  console.log(currentAnimation);
+  switch (currentAnimation) {
+    case 'ball1':
+      moveBall(sphere1);
+      break;
+    case 'delay':
+      delay();
+      break;
+    case 'ball2':
+      moveBall(sphere2);
+      break;
+  }
+}
 
 
 
 
-
-
-// let floorTexture2 = new THREE.TextureLoader().load('../img/checkerboard.png');
-// floorTexture2.wrapS = floorTexture2.wrapT = THREE.RepeatWrapping;
-// floorTexture2.repeat.set( 2, 2 );
-// var floorMaterial2 = new THREE.MeshBasicMaterial({
-//   map: floorTexture2,
-//   side: THREE.DoubleSide
-// });
-// var floorGeometry2 = new THREE.PlaneGeometry(200, 200, 1, 1);
-// var floor2 = new THREE.Mesh(floorGeometry2, floorMaterial2);
-// floor2.position.set(0,0,0);
-// floor2.rotation.x = 1.5;
-// scene.add(floor2);
-
-
-// MAKE SPHERE
-var sphereGeometry = new THREE.SphereGeometry(BALL_RADIUS, 30, 30);
-var sphereMaterial = new THREE.MeshLambertMaterial({color: 0xff0000});
-const sphere1 = new THREE.Mesh(sphereGeometry, sphereMaterial);
-const sphere2 = new THREE.Mesh(sphereGeometry, new THREE.MeshLambertMaterial({color: 0x00ff00}));
-sphere1.position.set(-20,30,-30);
-sphere2.position.set(20,30,-30);
-scene.add(sphere1);
-scene.add(sphere2);
-
-
-
-
-// var light = new THREE.AmbientLight(0xffffff, 0.5);
-var light1 = new THREE.PointLight(0xffffff, 1, 0);
-light1.position.set(0,50,-100);
-scene.add(light1);
-// scene.add(light);
 
 
 // RENDER ANIMATION
 let rotate = true;
 
 function animate() {
-  if (rotate) {
-    floor.rotation.x += 0.01;
-    wall.rotation.x += 0.01;
-    // cube.rotation.x += 0.01;
-    // cube2.rotation.x += 0.01;
-  }
-  if (floor.rotation.x > Math.PI/2) {
-    rotate = false;
-  }
+  // if (rotate) {
+  //   group.rotation.x += 0.005;
+  // }
+  // if (group.rotation.x > Math.PI/2) {
+  //   rotate = false;
+  // }
   // cube.rotation.y += 0.01;
-  ballToRight(sphere1);
+  // ballToRight(sphere2);
+  // moveBall1(sphere2);
+  doAnimation();
+  // animateCamera();
 	renderer.render( scene, camera );
   requestAnimationFrame( animate );
 }
 animate();
 
-
-
-
-var cubeMaterialArray = [];
-// order to add materials: x+,x-,y+,y-,z+,z-
-cubeMaterialArray.push( new THREE.MeshBasicMaterial( { color: 0xff3333, transparent: true,
-  wireframe: true } ) );
-cubeMaterialArray.push( new THREE.MeshBasicMaterial( { color: 0xff8800 } ) );
-cubeMaterialArray.push( new THREE.MeshBasicMaterial( { color: 0xffff33 } ) );
-cubeMaterialArray.push( new THREE.MeshBasicMaterial( { color: 0xff3333, transparent: true,
-  wireframe: true } ) );
-cubeMaterialArray.push( new THREE.MeshBasicMaterial( { color: 0x3333ff } ) );
-cubeMaterialArray.push( new THREE.MeshBasicMaterial( { color: 0x8833ff } ) );
-// var cubeMaterials = new THREE.MeshFaceMaterial( cubeMaterialArray );
-// Cube parameters: width (x), height (y), depth (z),
-//        (optional) segments along x, segments along y, segments along z
-var cubeGeometry = new THREE.CubeGeometry( 100, 100, 100, 1, 1, 1 );
-// using THREE.MeshFaceMaterial() in the constructor below
-//   causes the mesh to use the materials stored in the geometry
-const cube2 = new THREE.Mesh( cubeGeometry, cubeMaterialArray );
-// cube2.position.set(-100, 50, -50);
-// scene.add( cube2 );
-
+// CONTROLS
 controls = new THREE.OrbitControls(camera);
-
-cube2.rotation.x = 0;
-
-
-// window.addEventListener('keydown', (event) => {
-//   console.log('KEYDOWN', event)
-//   switch (event.key) {
-//     case 'ArrowUp':
-//       if (event.shiftKey) {
-//         camera.position.z += 10;
-//       } else {
-//         camera.rotation.x += .1;
-//       }
-//       break;
-//     case 'ArrowDown':
-//     if (event.shiftKey) {
-//       camera.position.z -= 10;
-//     } else {
-//       camera.rotation.x -= .1;
-//     }
-//       break;
-//     case 'ArrowRight':
-//       if (event.shiftKey) {
-//         camera.position.x += 1;
-//       } else {
-//         camera.rotation.y += .1;
-//       }
-//       break;
-//     case 'ArrowLeft':
-//       if (event.shiftKey) {
-//         camera.position.x -= 1;
-//       } else {
-//         camera.rotation.y -= .1;
-//       }
-//       break;
-//   }
-// })
